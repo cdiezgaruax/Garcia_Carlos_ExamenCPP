f9ca64c832bcc902082e0d3e905814e3d3a02e34
cdiezgaruax <cdiezgar@myuax.com>
2024-11-05 11:05:52 +0100

    main mejor y con un mini selecionadior

--- main.cpp
+++ main.cpp
@@ -4,15 +4,33 @@
 #include "eje3/eje3.h"
 #include "eje4/eje4.h"
 using namespace std;
+
 int main() {
+    int opcion;
+    cout << "Seleccione el ejercicio que desea ejecutar (1-4): "<<endl;
+    cout << "1. Identificacion de Numero Positivo, Negativo o Cero" << endl;
+    cout<< "2. Dia de la Semana"<<endl;
+    cout<< "3. Calculo de Promedio de Calificaciones"<<endl;
+    cout<< "4. Promocion de '3 por 2' en Tienda"<<endl;
+    cin >> opcion;
 
-    eje1();
-    eje2();
-    eje3();
-    eje4();
-
-
-
+    switch (opcion) {
+        case 1:
+            eje1();
+            break;
+        case 2:
+            eje2();
+            break;
+        case 3:
+            eje3();
+            break;
+        case 4:
+            eje4();
+            break;
+        default:
+            cout << "Opción inválida. Por favor seleccione un número del 1 al 4." << endl;
+            break;
+    }
 
     return 0;
 }
